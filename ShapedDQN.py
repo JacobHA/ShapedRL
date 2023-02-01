99b34447a8cc8f6329e60f9f2f9f50ccaf195715
JacobHA <jacobhadamczyk@gmail.com>
2023-02-01 15:48:55 -0500

    working sweep submission

---
+++ ShapedDQN.py
@@ -0,0 +1,89 @@
+import torch as th
+from torch.nn import functional as F
+from torch.optim import Adam
+import numpy as np
+
+from stable_baselines3.dqn import DQN
+
+
+class ShapedDQN(DQN):
+    """
+    DQN variant with shaped rewards at each Q update:
+    (^ denotes the current estimate as given by the FA.)
+    assuming V^ = max_a Q^(s,a), and that the environment is deterministic,
+    r --> r + gamma V^(s') - V(s)
+    """
+
+    def __init__(self, *args, shape: bool = False, minus_v=True, **kwargs):
+        super(ShapedDQN, self).__init__(*args, **kwargs)
+        self.shape = shape
+        self.minus_v = minus_v
+
+    def train(self, gradient_steps: int, batch_size: int = 100) -> None:
+        # Switch to train mode (this affects batch norm / dropout)
+        self.policy.set_training_mode(True)
+        # Update learning rate according to schedule
+        self._update_learning_rate(self.policy.optimizer)
+
+        losses = []
+        for _ in range(gradient_steps):
+            # Sample replay buffer
+            replay_data = self.replay_buffer.sample(
+                batch_size, env=self._vec_normalize_env)
+
+            with th.no_grad():
+                # Compute the next Q-values using the target network
+                next_q_value = self.q_net_target(replay_data.next_observations)
+                # min_q_value, idx_min = next_q_value.min(dim=1, keepdim=True)
+                # Follow greedy policy: use the one with the highest value
+                max_q_value, idx_max = next_q_value.max(dim=1, keepdim=True)
+                # Avoid potential broadcast issue
+                max_q_value = max_q_value.reshape(-1, 1)
+                # 1-step TD target
+                target_q_values = replay_data.rewards + \
+                    (1 - replay_data.dones) * self.gamma * max_q_value
+                # clip target to be between min and max values
+                # get probabilities of taking the min and max actions
+                curr_q_values = self.policy.q_net(replay_data.observations)
+                next_q_values = self.policy.q_net(
+                    replay_data.next_observations)
+                # curr_v_min, _ = curr_q_values.min(dim=1, keepdim=True)
+                curr_v_max, _ = curr_q_values.max(dim=1, keepdim=True)
+                # next_v_min, _ = next_q_values.min(dim=1, keepdim=True)
+                next_v_max, _ = next_q_values.max(dim=1, keepdim=True)
+
+                rewards = replay_data.rewards
+                if self.shape:
+                    rewards += self.gamma * next_v_max - curr_v_max
+
+                target_q_values = rewards + \
+                    (1 - replay_data.dones) * self.gamma * max_q_value
+
+                # if self.minus_v:
+                #     target_q_values += curr_v_max
+
+            # Get current Q-values estimates
+            current_q_values = self.q_net(replay_data.observations)
+
+            # Retrieve the q-values for the actions from the replay buffer
+            current_q_values = th.gather(
+                current_q_values, dim=1, index=replay_data.actions.long())
+
+            # Compute Huber loss (less sensitive to outliers)
+            loss = F.smooth_l1_loss(current_q_values, target_q_values)
+            losses.append(loss.item())
+
+            # Optimize the policy
+            self.policy.optimizer.zero_grad()
+            loss.backward()
+            # Clip gradient norm
+            th.nn.utils.clip_grad_norm_(
+                self.policy.parameters(), self.max_grad_norm)
+            self.policy.optimizer.step()
+
+        # Increase update counter
+        self._n_updates += gradient_steps
+
+        self.logger.record("train/n_updates",
+                           self._n_updates, exclude="tensorboard")
+        self.logger.record("train/loss", np.mean(losses))
---
+++ experiment.py
@@ -0,0 +1,58 @@
+import wandb
+import gym
+from stable_baselines3.dqn.dqn import DQN
+from ShapedSAC import ShapedSAC
+from wandb.integration.sb3 import WandbCallback
+import argparse
+
+
+def shaping(sweepid, apply_shaping=True, env_name="FrozenLake-v1",
+            learning_rate=0.0001,
+            gamma=0.99, ent_coef='auto',
+            is_slippery=False, train_steps=1000000):
+
+    env = gym.make(env_name)
+
+    with wandb.init(
+            config={
+                "env_name": env_name,
+                "shaped": apply_shaping,
+                "is_slippery": is_slippery,
+            },
+            sync_tensorboard=True
+    ) as run:
+
+        cb = WandbCallback(
+            gradient_save_freq=15000,
+            model_save_path=f"models/{env_name}.pkl",
+            verbose=2,
+        )
+        cb = None
+
+        model = ShapedSAC(
+            policy="MlpPolicy",
+            env=env,
+            learning_rate=learning_rate,  # config.learning_rate,
+            gamma=gamma,
+            buffer_size=50000,
+            ent_coef=ent_coef,
+            shape=apply_shaping,
+            tensorboard_log="./logs/",)
+
+        model.learn(total_timesteps=train_steps, callback=cb)
+
+
+if __name__ == "__main__":
+    # Parse in the sweepid args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--sweepid', type=str, default=None)
+    args = parser.parse_args()
+    sweepid = args.sweepid
+
+    def wandb_func():
+        # Based on https://openreview.net/pdf?id=HJjvxl-Cb
+        shaping(sweepid, apply_shaping=1, env_name='Swimmer-v3',
+                learning_rate=3e-4, train_steps=1_500_000, gamma=0.99, ent_coef=1/100)
+
+    wandb.agent(sweepid, function=wandb_func, count=1)
+    wandb.finish()
