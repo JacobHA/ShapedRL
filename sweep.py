764f59e0c64aeb0ef2ffd279f3fa3e5d80581f00
BobMak <vlmr.mko@gmali.com>
2023-04-03 23:18:04 -0700

    fixed sweep eval

--- sweep.py
+++ sweep.py
@@ -1,7 +1,10 @@
 import argparse
 import json
+
+from gym.wrappers import FrameStack
 from stable_baselines3.common.atari_wrappers import AtariWrapper
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv, VecEnv, VecFrameStack, VecNormalize, VecMonitor
+from stable_baselines3.common.env_util import Monitor
 from stable_baselines3.common.callbacks import EvalCallback
 import gym
 import wandb
@@ -41,9 +44,11 @@ same_dqn_atari_hparams = {
 def get_hparams(**hyperparams):
     envname = "AlmostEverythingWithNoFrameskip-v4" if env_name in same_dqn_atari_hparams else env_name
     with open("hparams.json") as f:
-        config = json.load(f)[algo][envname]
-    config.update(hyperparams)
-    return config
+        conf = json.load(f)
+        env_config = conf[algo][envname]['env']
+        model_config = conf[algo][envname]['model']
+    model_config.update(hyperparams)
+    return env_config, model_config
 
 
 def _make_env(hparams):
@@ -55,7 +60,7 @@ def _make_env(hparams):
     return _init
 
 
-def make_env(hparams):
+def make_train_env(hparams):
     env = SubprocVecEnv([_make_env(hparams) for _ in range(n_envs)])
     env = VecMonitor(env)
     if "frame_stack" in hparams:
@@ -65,28 +70,36 @@ def make_env(hparams):
     return env
 
 
-def make_model(env, hparams):
-    del [hparams["frame_stack"], hparams["env_wrapper"], hparams["n_timesteps"], hparams["normalize"]]
-    return ShapedDQN(env=env, **hparams)
+def make_eval_env(hparams, run=None):
+    env = DummyVecEnv([_make_env(hparams)])
+    env = VecMonitor(env, f'./runs/{run.id}' if run else None)
+    if "frame_stack" in hparams:
+        env = VecFrameStack(env, hparams["frame_stack"])
+    return env
+
+
+def atari(dict_cfg, run=None):
+    env_hparams, model_hparams = get_hparams(**dict_cfg)
+    env = make_train_env(env_hparams)
+    n_time_steps = model_hparams.pop("n_timesteps")
+    model = ShapedDQN(env=env, **model_hparams)
+    model_name = f"{algo}-{env_name}-{'shaped' if model_hparams['do_shape'] else 'unshaped'}"
+
+    eval_env = make_eval_env(env_hparams, run)
+    eval_callback = EvalCallback(eval_env, n_eval_episodes=1,
+                                 log_path=f'./runs/{run.id if run else "default"}',
+                                 eval_freq=15_000,
+                                 deterministic=True,
+                                 best_model_save_path=f'./best_model/{model_name}')
+
+    model.learn(n_time_steps, log_interval=10, tb_log_name="runs", callback=eval_callback)
 
 
 def wandb_atari():
     with wandb.init(sync_tensorboard=True) as run:
         cfg = run.config
         dict_cfg = cfg.as_dict()
-        hparams = get_hparams(**dict_cfg)
-        n_time_steps = hparams["n_timesteps"]
-        env = make_env(hparams)
-        model = make_model(env, hparams)
-
-        eval_env = gym.make(env_name)
-        eval_callback = EvalCallback(eval_env, n_eval_episodes=1,
-                                     log_path=f'./runs/{run.id}',
-                                     eval_freq=15_000,
-                                     deterministic=True,
-                                     best_model_save_path=f'./best_model/{run.id}')
-
-        model.learn(n_time_steps, log_interval=10, tb_log_name="runs", callback=eval_callback)
+        atari(dict_cfg, run)
 
 
 if __name__ =="__main__":
@@ -108,4 +121,5 @@ if __name__ =="__main__":
     env_name = args.env
     algo = args.algo
     n_envs = args.nenvs
-    wandb.agent(sweep_id, function=wandb_atari, count=args.number, project=args.project)
+    # wandb.agent(sweep_id, function=wandb_atari, count=args.number, project=args.project)
+    atari({'do_shape': False}, None)
