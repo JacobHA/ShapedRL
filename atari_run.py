6810924a7fccd9f28761ac0e9937bcb62f4be1b0
vlad <vlmr.mko@gmail.com>
2024-02-21 13:43:07 -0800

    updated run to use good sb3 atari wrappers

--- atari_run.py
+++ atari_run.py
@@ -9,14 +9,15 @@ from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.vec_env import vec_transpose, vec_frame_stack
 from gymnasium.wrappers.atari_preprocessing import AtariPreprocessing
 
-env_str = "PongNoFrameskip-v4"
+# env_str = "PongNoFrameskip-v4"
+env_str = "SkiingNoFrameskip-v4"
 # There already exists an environment generator that will make and wrap atari environments correctly.
 
 # env_str = "BreakoutNoFrameskip-v4"
 from all_atari_envs import atari_env_strs
-idx = 9
-env_str = atari_env_strs[idx]
-env_str = "MontezumaRevengeNoFrameskip-v4"
+# idx = 9
+# env_str = atari_env_strs[idx]
+# env_str = "MontezumaRevengeNoFrameskip-v4"
 env = make_atari_env(env_str, n_envs=1, seed=42)
 # Stack 4 frames
 env = VecFrameStack(env, n_stack=4)
@@ -49,15 +50,15 @@ eval_callback = EvalCallback(eval_env, n_eval_episodes=5,
                 deterministic=True)
 
 
-wandb.init(project='bs-rs', entity='jacobhadamczyk', sync_tensorboard=True)
-wandb.log({'env_id': env_str, 'shaping_mode': shaping_mode, 'use_dones': use_dones})
+# wandb.init(project='bs-rs', entity='jacobhadamczyk', sync_tensorboard=True)
+# wandb.log({'env_id': env_str, 'shaping_mode': shaping_mode, 'use_dones': use_dones})
 
-model = ShapedDQN("CnnPolicy", env, shaping_mode=shaping_mode, 
+model = ShapedDQN("CnnPolicy", env, do_shape=True, no_done_mask=True,
                   use_oracle=False,
                   verbose=4, tensorboard_log="./runs", **hparams, device='cuda')
 total_timesteps = 10_000_000
 model.learn(total_timesteps, log_interval=100, callback=eval_callback, tb_log_name=f"{shaping_mode}")
 
-wandb.finish()
+# wandb.finish()
 
 # model.save_final_model(f"{env_str}.pth")
--- run.py
+++ run.py
@@ -3,6 +3,8 @@ import json
 import gymnasium as gym
 from gymnasium.wrappers.atari_preprocessing import AtariPreprocessing
 from stable_baselines3.common.atari_wrappers import FireResetEnv
+from stable_baselines3.common.env_util import make_atari_env
+from stable_baselines3.common.vec_env import VecFrameStack, VecTransposeImage
 
 from wrappers import FrameStack, PermuteAtariObs
 
@@ -14,6 +16,7 @@ import argparse
 
 
 atari_envs = {"AdventureNoFrameskip-v4", "AirRaidNoFrameskip-v4", "AlienNoFrameskip-v4", "AmidarNoFrameskip-v4", "AssaultNoFrameskip-v4", "AsterixNoFrameskip-v4", "AsteroidsNoFrameskip-v4", "AtlantisNoFrameskip-v4", "Atlantis2NoFrameskip-v4", "BackgammonNoFrameskip-v4", "BankHeistNoFrameskip-v4", "BasicMathNoFrameskip-v4", "BattleZoneNoFrameskip-v4", "BeamRiderNoFrameskip-v4", "BerzerkNoFrameskip-v4", "BlackjackNoFrameskip-v4", "BowlingNoFrameskip-v4", "BoxingNoFrameskip-v4", "BreakoutNoFrameskip-v4", "CarnivalNoFrameskip-v4", "CasinoNoFrameskip-v4", "CentipedeNoFrameskip-v4", "ChopperCommandNoFrameskip-v4", "CrazyClimberNoFrameskip-v4", "CrossbowNoFrameskip-v4", "DarkchambersNoFrameskip-v4", "DefenderNoFrameskip-v4", "DemonAttackNoFrameskip-v4", "DonkeyKongNoFrameskip-v4", "DoubleDunkNoFrameskip-v4", "EarthworldNoFrameskip-v4", "ElevatorActionNoFrameskip-v4", "EnduroNoFrameskip-v4", "EntombedNoFrameskip-v4", "EtNoFrameskip-v4", "FishingDerbyNoFrameskip-v4", "FlagCaptureNoFrameskip-v4", "FreewayNoFrameskip-v4", "FroggerNoFrameskip-v4", "FrostbiteNoFrameskip-v4", "GalaxianNoFrameskip-v4", "GopherNoFrameskip-v4", "GravitarNoFrameskip-v4", "HangmanNoFrameskip-v4", "HauntedHouseNoFrameskip-v4", "HeroNoFrameskip-v4", "HumanCannonballNoFrameskip-v4", "IceHockeyNoFrameskip-v4", "JamesbondNoFrameskip-v4", "JourneyEscapeNoFrameskip-v4", "KaboomNoFrameskip-v4", "KangarooNoFrameskip-v4", "KeystoneKapersNoFrameskip-v4", "KingKongNoFrameskip-v4", "KlaxNoFrameskip-v4", "KoolaidNoFrameskip-v4", "KrullNoFrameskip-v4", "KungFuMasterNoFrameskip-v4", "LaserGatesNoFrameskip-v4", "LostLuggageNoFrameskip-v4", "MarioBrosNoFrameskip-v4", "MiniatureGolfNoFrameskip-v4", "MontezumaRevengeNoFrameskip-v4", "MrDoNoFrameskip-v4", "MsPacmanNoFrameskip-v4", "NameThisGameNoFrameskip-v4", "OthelloNoFrameskip-v4", "PacmanNoFrameskip-v4", "PhoenixNoFrameskip-v4", "PitfallNoFrameskip-v4", "Pitfall2NoFrameskip-v4", "PongNoFrameskip-v4", "PooyanNoFrameskip-v4", "PrivateEyeNoFrameskip-v4", "QbertNoFrameskip-v4", "RiverraidNoFrameskip-v4", "RoadRunnerNoFrameskip-v4", "RobotankNoFrameskip-v4", "SeaquestNoFrameskip-v4", "SirLancelotNoFrameskip-v4", "SkiingNoFrameskip-v4", "SolarisNoFrameskip-v4", "SpaceInvadersNoFrameskip-v4", "SpaceWarNoFrameskip-v4", "StarGunnerNoFrameskip-v4", "SupermanNoFrameskip-v4", "SurroundNoFrameskip-v4", "TennisNoFrameskip-v4", "TetrisNoFrameskip-v4", "TicTacToe3DNoFrameskip-v4", "TimePilotNoFrameskip-v4", "TrondeadNoFrameskip-v4", "TurmoilNoFrameskip-v4", "TutankhamNoFrameskip-v4", "UpNDownNoFrameskip-v4", "VentureNoFrameskip-v4", "VideoCheckersNoFrameskip-v4", "VideoChessNoFrameskip-v4", "VideoCubeNoFrameskip-v4", "VideoPinballNoFrameskip-v4", "WizardOfWorNoFrameskip-v4", "WordZapperNoFrameskip-v4", "YarsRevengeNoFrameskip-v4", "ZaxxonNoFrameskip-v4"}
+EVAL_FREQ = 50_000
 
 
 def run(env_str, hparams, total_timesteps, log_freq, device='cuda', log_dir="./runs"):
@@ -24,28 +27,14 @@ def run(env_str, hparams, total_timesteps, log_freq, device='cuda', log_dir="./r
         det=True
 
     if env_str in atari_envs:
-        framestack_k = 4
-        frameskip = 4
-        render=False
-        permute_dims = False
-        grayscale_obs = True
-        env = gym.make(env_str, frameskip=frameskip)
-        env = AtariPreprocessing(env, terminal_on_life_loss=True, screen_size=84, grayscale_obs=grayscale_obs,
-                                 grayscale_newaxis=True, scale_obs=False, noop_max=30, frame_skip=1)
-        if framestack_k:
-            env = FrameStack(env, framestack_k)
-        # permute dims for nature CNN in sb3
-        if permute_dims:
-            env = PermuteAtariObs(env)
-        eval_env = gym.make(env_str, render_mode='human' if render else None, frameskip=frameskip)
-        eval_env = AtariPreprocessing(eval_env, terminal_on_life_loss=True, screen_size=84, grayscale_obs=grayscale_obs,
-                                      grayscale_newaxis=True, scale_obs=False, noop_max=30, frame_skip=1)
-        if framestack_k:
-            eval_env = FrameStack(eval_env, framestack_k)
-        if permute_dims:
-            eval_env = PermuteAtariObs(eval_env)
-        env = FireResetEnv(env)
-        eval_env = FireResetEnv(eval_env)
+        env = make_atari_env(env_str, n_envs=1, seed=42)
+        # Stack 4 frames
+        env = VecFrameStack(env, n_stack=4)
+        env = VecTransposeImage(env)
+        # eval
+        eval_env = make_atari_env(env_str, n_envs=1, seed=0)
+        eval_env = VecFrameStack(eval_env, n_stack=4)
+        eval_env = VecTransposeImage(eval_env)
         policy = "CnnPolicy"
     else:
         env = gym.make(env_str, **env_kwargs)
@@ -54,7 +43,7 @@ def run(env_str, hparams, total_timesteps, log_freq, device='cuda', log_dir="./r
 
     eval_callback = EvalCallback(eval_env, n_eval_episodes=20,
                     log_path=f'./runs/',
-                    eval_freq=log_freq*10,
+                    eval_freq=EVAL_FREQ,
                     deterministic=True,
                     verbose=1,)
 
@@ -73,7 +62,7 @@ if __name__ == "__main__":
     parser.add_argument("-s", type=bool, default=True)
     parser.add_argument("-t", type=bool, default=False,
                         help="If true, the done mask is not applied to the target Q value")
-    parser.add_argument("-e", "--env", type=str, default="FrozenLake-v1")  # "PongNoFrameskip-v4"
+    parser.add_argument("-e", "--env", type=str, default="PongNoFrameskip-v4")  # "FrozenLake-v1" "PongNoFrameskip-v4"
     args = parser.parse_args()
     do_shape = args.s
     no_done_mask = args.t
